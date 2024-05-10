6bb3e7a2b35d6aafb49fa6f6230e023ddd892038
sam11022112 <sam11022112@gmail.com>
2024-05-10 18:55:30 +0530

    Add files via upload

---
+++ showuser.component.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-showuser',
+  templateUrl: './showuser.component.html',
+  styleUrl: './showuser.component.css'
+})
+export class ShowuserComponent {
+user_records:any[]=[];
+data={
+  name:"",
+  email:"",
+  mobile:"",
+  password:""
+}
+constructor(){
+  this.user_records=JSON.parse(localStorage.getItem('users')||'{}')
+}
+}
